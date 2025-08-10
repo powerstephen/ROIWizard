ef8c81fd59b53ec746b19b0c9c25d3467a970122
powerstephen <power.stephen@gmail.com>
2025-08-11 05:07:16 +0800

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,29 @@
 import React from 'react'
-import logoUrl from '/tilla-logo.svg'
-export const Header=({onShare,onExport})=>(<div className='flex items-center justify-between'>
-  <div className='flex items-center gap-3'><img src={logoUrl} alt='Tilla' className='h-8 w-auto'/><span className='badge'>ROI Wizard</span></div>
-  <div className='flex items-center gap-2'><button onClick={onShare} className='btn'>Share link</button><button onClick={onExport} className='btn'>Export CSV</button><a href='https://www.tilla.technology' target='_blank' rel='noreferrer' className='btn btn-primary'>Learn about Tilla</a></div>
-</div>)
+
+export const Header = ({ onShare, onExport }) => (
+  <div className='flex items-center justify-between py-4'>
+    <div className='flex items-center gap-4'>
+      {/* Bigger logo */}
+      <img src="/tilla-logo.svg" alt="Tilla" className="h-20 w-auto" />
+      
+      {/* Short, sharp tagline */}
+      <span className='text-xl font-semibold text-[#0B00CF] leading-tight'>
+        Your Crew-Ops ROI, Calculated
+      </span>
+    </div>
+
+    <div className='flex items-center gap-2'>
+      <button onClick={onShare} className='btn'>Share link</button>
+      <button onClick={onExport} className='btn'>Export CSV</button>
+      <a
+        href='https://www.tilla.tech/'
+        target='_blank'
+        rel='noreferrer'
+        className='btn btn-primary'
+      >
+        Learn about Tilla
+      </a>
+    </div>
+  </div>
+)
+
